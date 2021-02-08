4280862746e516e11c3bd88ecd3545b2d708421e
Adib Ahsan Chowdhury <adibc@hsenidmobile.com>
2021-02-08 12:34:43 +0600

    intial changes

---
+++ src/test/java/com/adib/websockettest/WebsockettestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adib.websockettest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsockettestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
