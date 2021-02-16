8d23638cd61349063b5661b465c0678adb9c21ab
Adib Ahsan Chowdhury <adibc@hsenidmobile.com>
2021-02-16 13:42:05 +0600

    stomp websocket

---
+++ src/main/java/com/adib/websockettest/stompwebsocket/WebSocketBroadcastController.java
@@ -0,0 +1,21 @@
+package com.adib.websockettest.stompwebsocket;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebSocketBroadcastController {
+
+    @GetMapping("/stomp-broadcast")
+    public String getWebSocketBroadcast() {
+        return "stomp-broadcast";
+    }
+
+    @MessageMapping("/broadcast")
+    @SendTo("/topic/messages")
+    public ChatMessage send(ChatMessage chatMessage) throws Exception {
+        return new ChatMessage(chatMessage.getFrom(), chatMessage.getText(), "ALL");
+    }
+}
